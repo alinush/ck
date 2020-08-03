3e9002b4a6f161a4c18a9cbf6afd11c555d5bccc
aluex <aluex@outlook.com>
2020-08-03 13:21:48 -0400

    minor fix for acmdl handler.

--- citationkeys/urlhandlers.py
+++ citationkeys/urlhandlers.py
@@ -83,7 +83,7 @@ def download_pdf_andor_bib(opener, user_agent, pdfurl, biburl, verbosity):
 def dlacm_handler(opener, soup, parsed_url, ck_bib_dir, parser, user_agent, verbosity):
     path = parsed_url.path.split('/')[2:]
     if len(path) > 1:
-        doi = path[0] + '/' + path[1]
+        doi = path[-2] + '/' + path[-1]
     elif 'doid' in parsed_url.query:
         doi = '10.1145/' + parsed_url.query.split('=')[1] # 10.1145/doid
     else:
