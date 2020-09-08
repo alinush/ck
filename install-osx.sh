4889401830ddefab4f93dbe0b16262c2c8a2a10d
Alin Tomescu <alinush@mit.edu>
2020-09-07 20:04:47 -0700

    make sure brew is installed in install-osx.sh

--- install-osx.sh
+++ install-osx.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+which brew &>/dev/null || { echo "ERROR: 'brew' is not installed"; exit 1; }
+
 comp_dir=`brew --prefix`/etc/bash_completion.d/
 
 # Does NOT overwrite existing files
